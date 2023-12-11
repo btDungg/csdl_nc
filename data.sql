3bcb0577608b4f8cf5773e96074986ec0c85611c
Doen1x <thituyetnguyen.dmxtk@gmail.com>
2023-12-11 16:09:26 +0700

    phan cong dieu tri

--- data.sql
+++ data.sql
@@ -8,3 +8,10 @@ create table CuocHen
 	  TinhTrang nvarchar(12) CONSTRAINT CK_TT CHECK (TinhTrang IN (N'Tái khám', N'Cuộc hẹn mới'))
 )
 
+CREATE TABLE PhanCongDieuTri
+(
+	MaNhaSi char(6) NOT NULL,
+	MaKeHoach char(10) NOT NULL,
+	VaiTro nvarchar(10),
+	PRIMARY KEY(MaNhaSi,MaKeHoach)
+)
