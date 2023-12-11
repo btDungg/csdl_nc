5d0b01616112340606bd86555bad455fb42fe868
Doen1x <thituyetnguyen.dmxtk@gmail.com>
2023-12-11 16:29:24 +0700

    16,17,18,19

--- data.sql
+++ data.sql
@@ -8,17 +8,49 @@ create table CuocHen
 	  TinhTrang nvarchar(12) CONSTRAINT CK_TT CHECK (TinhTrang IN (N'Tái khám', N'Cuộc hẹn mới'))
 )
 
-CREATE TABLE PhanCongDieuTri
-(
-	MaNhaSi char(6) NOT NULL,
-	MaKeHoach char(10) NOT NULL,
-	VaiTro nvarchar(10),
-	PRIMARY KEY(MaNhaSi,MaKeHoach)
-)
+
 
 create table PhongKham
 (
      MaPhong char(4),
 	 DiaChi nvarchar(50) NOT NULL
 
+)
+
+
+
+--16
+CREATE TABLE PhanCongDieuTri
+(
+	MaNhaSi char(6) NOT NULL,
+	MaKeHoach char(10) NOT NULL FOREIGN KEY REFERENCES KeHoachDieuTri(MaKeHoach),
+	VaiTro nvarchar(10) NOT NULL,
+	PRIMARY KEY(MaNhaSi,MaKeHoach)
+)
+
+--19
+CREATE TABLE Rang
+(
+	MaRang char(5) NOT NULL PRIMARY KEY,
+	TenRang nvarchar(20) NOT NULL
+)
+
+--18
+CREATE TABLE BeMatRang
+(
+	MaMatRang char(13) NOT NULL,
+	MaRang char(5) NOT NULL FOREIGN KEY REFERENCES Rang(MaRang),
+	TenMatRang nvarchar(20) NOT NULL,
+	PRIMARY KEY (MaMatRang,MaRang)
+)
+
+--17
+CREATE TABLE RangDieuTri
+(
+	MaRang char(5) NOT NULL,
+	MaMatRang char(13) NOT NULL,
+	MaKeHoach char(10) NOT NULL FOREIGN KEY REFERENCES KeHoachDieuTri(MaKeHoach),
+	PRIMARY KEY(MaRang,MaMatRang,MaKeHoach),
+	FOREIGN KEY(MaRang,MaMatRang)
+	REFERENCES BeMatRang(MaRang,MaMatRang)
 )
