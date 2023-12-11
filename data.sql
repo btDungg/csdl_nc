3f40334113f85e545a2b8ab8e7dc6e63380d61f3
21120470 <nhathuynguyen217@gmail.com>
2023-12-11 17:07:19 +0700

    6, 7, 8, 9, 10

--- data.sql
+++ data.sql
@@ -1,11 +1,11 @@
-﻿
+﻿--6--
 create table CuocHen 
 (
-      MaCuocHen char(12),
-	  MaNguoiTao char(6),
-	  MaBenhNhan char(12),
+      MaCuocHen char(12) PRIMARY KEY, 
+	  MaNguoiTao char(6) FOREIGN KEY REFERENCES NguoiDung(MaNguoiDung),
+	  MaBenhNhan char(12) FOREIGN KEY REFERENCES HoSoBenhNhan(MaBenhNhan),
 	  ThoiGian DateTime NOT NULL ,
-	  TinhTrang nvarchar(12) CONSTRAINT CK_TT CHECK (TinhTrang IN (N'Tái khám', N'Cuộc hẹn mới'))
+	  TinhTrang nvarchar(12) CHECK (TinhTrang IN (N'Tái khám', N'Cuộc hẹn mới'))
 )
 
 
@@ -26,14 +26,42 @@ CREATE TABLE ChongChiDinh
 	PRIMARY KEY(MaThuoc,MaBenhNhan)
 )
 
+--7--
 create table PhongKham
 (
-     MaPhong char(4),
+     MaPhong char(4) primary key,
 	 DiaChi nvarchar(50) NOT NULL
-
 )
 
+--8--
+create table ChiTietNoiLamViec
+(
+    MaNhaSi char(6) FOREIGN KEY REFERENCES NhaSi(MaNhaSi),
+	MaPhong char(4) FOREIGN KEY REFERENCES PhongKham(MaPhong),
+	NgayLamViec date not null,
+	primary key(MaNhaSi, MaPhong)
+)
 
+--9--
+create table DonThuoc 
+(
+   MaThuoc char(12) FOREIGN KEY REFERENCES Thuoc(MaThuoc),
+   MaBenhNhan char(12) FOREIGN KEY REFERENCES HoSoBenhNhan(MaBenhNhan),
+   SoLuong int not null,
+   TongTien float NOT NULL CHECK (TongTien >= 0),
+   primary key (MaThuoc, MaBenhNhan)
+)
+
+--10--
+create table Thuoc
+(
+    MaThuoc char(12) primary key,
+	TenThuoc varchar(35) not null,
+	DonVi nvarchar(5) not null,
+	SoLuongTon int not null check (SoLuongTon >=0),
+	NgayHetHan date not null,
+	GiaThuoc float not null check (GiaThuoc > 0)
+)
 
 --16
 CREATE TABLE PhanCongDieuTri
