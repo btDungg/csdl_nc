143aacc862fcc302fadb6a80afae3862f5456c47
21120470 <nhathuynguyen217@gmail.com>
2023-12-11 15:59:36 +0700

    tao bang CuocHen

--- data.sql
+++ data.sql
@@ -1,1 +1,10 @@
-hello world 
+﻿
+create table CuocHen 
+(
+      MaCuocHen char(12),
+	  MaNguoiTao char(6),
+	  MaBenhNhan char(12),
+	  ThoiGian DateTime,
+	  TinhTrang nvarchar(12) CONSTRAINT CK_TT CHECK (TinhTrang IN (N'Tái khám', N'Cuộc hẹn mới'))
+)
+
