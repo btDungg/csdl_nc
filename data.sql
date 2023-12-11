04f329c15c32790e29e8d69b0a12925b3bcbee64
TheDuyet <theduyetkg@gmail.com>
2023-12-11 16:08:41 +0700

    create table NguoiDung

--- data.sql
+++ data.sql
@@ -8,3 +8,12 @@ create table CuocHen
 	  TinhTrang nvarchar(12) CONSTRAINT CK_TT CHECK (TinhTrang IN (N'Tái khám', N'Cuộc hẹn mới'))
 )
 
+create table NguoiDung
+(
+	MaNguoiDung char(6) primary key,
+	HoTen nvarchar(35) not null,
+	TaiKhoan varchar(12) unique not null,
+	MatKhau varchar(30) not null,
+	LoaiNguoiDung varchar(3)
+)
+
